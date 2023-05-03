87df0bf5034f46b8273aab19f6ab14e3022bc09f
Aaron Sala <salaaron2@gmail.com>
2023-05-04 01:29:15 +0300

    modifield

--- src/App.js
+++ src/App.js
@@ -10,6 +10,34 @@ function App(props) {
   function addTask(name){
    const newTask = {id:`todo-${nanoid()}`, name,completed:false};
    setTasks([...tasks, newTask]);
+
+  }
+  
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+  function deleteTask(id){
+    const remainingTasks = tasks.filter((task) => id !==task.id);
+    setTasks(remainingTasks);
   }
   const taskList = tasks.map((task) =>(
    <Todo 
@@ -17,20 +45,27 @@ function App(props) {
      name={task.name}
      completed ={task.completed}
      key={task.id}
+     toggleTaskCompleted = {toggleTaskCompleted}
+     deleteTask={deleteTask}
+     editTask = {editTask}
    />
   )
 );
+const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
+    
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-     
+    
        <Form addTask={addTask} />
      
       <div className="filters btn-group stack-exception">
         <FilterButton name ={props.name}/>
         
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         
         className="todo-list stack-large stack-exception"
